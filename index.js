60bd71e452f05c9f315f4c328019d11e1c319f94
Saquib Asghar <saq9516@gmail.com>
2024-08-03 22:41:12 +0530

    add react, react-dom via npm, start app with Parcel

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("This is a test file");
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const children = React.createElement(
+  "div",
+  {},
+  React.createElement("span", {}, [
+    React.createElement("h1", {}, "React With Parcel ⚛️ + 📦 = 🚀"),
+    React.createElement(
+      "p",
+      {},
+      "This is a simple example of React application bundled with Parcel."
+    ),
+    React.createElement("a", { href: "https://parceljs.org/" }, "Learn More"),
+    React.createElement("br", {}),
+  ])
+);
+const rootElement = document.getElementById("root");
+
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(children);
