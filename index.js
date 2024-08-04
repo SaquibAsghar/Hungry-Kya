869ee3296d81ec3c90aa43ec77d7ad8f20362fd7
Saquib Asghar <saq9516@gmail.com>
2024-08-04 12:24:57 +0530

    add npm cmd, use component composition

--- index.js
+++ index.js
@@ -1,22 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./src/App";
 
-const children = React.createElement(
-  "div",
-  {},
-  React.createElement("span", {}, [
-    React.createElement("h1", {}, "React With Parcel ⚛️ + 📦 = 🚀"),
-    React.createElement(
-      "p",
-      {},
-      "This is a simple example of React application bundled with Parcel."
-    ),
-    React.createElement("a", { href: "https://parceljs.org/" }, "Learn More"),
-    React.createElement("br", {}),
-  ])
-);
 const rootElement = document.getElementById("root");
 
 const root = ReactDOM.createRoot(rootElement);
 
-root.render(children);
+root.render(<App />);
---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Header = () => <h1>React With Parcel ⚛️ + 📦 = 🚀 </h1>;
+
+const Description = () => (
+  <>
+    <p>This is a simple example of React application bundled with Parcel.</p>
+    <a href="https://react.dev/" target="blank">
+      Learn React
+    </a>
+    &nbsp;
+    <a href="https://parceljs.org/" target="blank">
+      Learn Parcel
+    </a>
+    <br />
+    <h3>This is a simple example of React application bundled with Parcel</h3>
+  </>
+);
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Description />
+    </div>
+  );
+};
+
+export default App;
